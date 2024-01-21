b76b7ef20ae17d031f2c6196ba9ff4177d1065ce
Ante Bucan <ab8390@g.rit.edu>
2024-01-21 16:48:12 +0100

    new changes

--- ts/form-schemas/form-schemas.ts
+++ ts/form-schemas/form-schemas.ts
@@ -1,6 +1,10 @@
 import * as z from 'zod';
 import { AccountColor } from '../enums/app_enums';
-import { AccountType, RecordType, CategoryType } from '@prisma/client';
+import {
+  AccountType,
+  RecordType,
+  CategoryType,
+} from '@prisma/client';
 
 export const accountSchema = z.object({
   name: z.string().min(3, {
@@ -19,25 +23,4 @@ export const recordSchema = z.object({
   category: z.nativeEnum(CategoryType),
   accountId: z.string(),
   createdAt: z.any(),
-});
-
-export const registerSchema = z.object({
-  username: z
-    .string()
-    .min(3, { message: 'Name is too short' })
-    .max(20, 'Username is too long'),
-  email: z
-    .string()
-    .min(1, 'Email is required')
-    .email({ message: 'Invalid email address' }),
-  password: z.string().min(1, 'Password is required').min(6, {
-    message: 'Password must be at least 6 characters long',
-  }),
-});
-
-export const loginSchema = z.object({
-  email: z.string().email({ message: 'Invalid email address' }),
-  password: z.string().min(6, {
-    message: 'Password must be at least 6 characters long',
-  }),
 });
