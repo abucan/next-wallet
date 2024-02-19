9197b49b46da4c4995a47263b4775d0ae7c7dcdc
Ante Bucan <ab8390@g.rit.edu>
2024-02-19 15:09:56 +0100

    fixed sidebar

--- app/(dashboard)/_components/main-sidebar.tsx
+++ app/(dashboard)/_components/main-sidebar.tsx
@@ -3,7 +3,7 @@ import { SidebarRoutes } from './sidebar-routes';
 
 export const Sidebar = () => {
   return (
-    <div className='h-full flex flex-col overflow-y-auto bg-sidebar-color items-center pt-5 border-r'>
+    <div className='h-full flex flex-col overflow-y-auto bg-sidebar-color items-center pt-5 md:border-r'>
       <div className='p-3'>
         <Logo width={130} />
       </div>
--- app/(dashboard)/_components/mobile-sidebar.tsx
+++ app/(dashboard)/_components/mobile-sidebar.tsx
@@ -1,10 +1,21 @@
+'use client';
+
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
 import { Menu } from 'lucide-react';
 import { Sidebar } from './main-sidebar';
+import { useEffect, useState } from 'react';
+import { usePathname } from 'next/navigation';
 
 export const MobileSidebar = () => {
+  const [open, setIsOpen] = useState(false);
+  const pathname = usePathname();
+
+  useEffect(() => {
+    setIsOpen(false);
+  }, [pathname]);
+
   return (
-    <Sheet>
+    <Sheet open={open} onOpenChange={setIsOpen}>
       <SheetTrigger className='md:hidden hover:opacity-75 transition'>
         <Menu />
       </SheetTrigger>
